7a6caf7ce25f98fc397823034f852912425f64c8
Anjali Sangekar <99080406+AnjaliSangekar@users.noreply.github.com>
2024-07-09 18:29:00 +0530

    Add files via upload

---
+++ AnonymousTypeDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new { 
+                Id = 1, 
+                FirstName = "John", 
+                LastName = "Smith", 
+                Address = new { Id = 101, City = "pune", Country = "India" }
+            };
+            Console.WriteLine(student.Id);
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.LastName);
+            Console.WriteLine(student.Address.City + " " + student.Address.Country);
+        }
+    }
+}
